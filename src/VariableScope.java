b87a400ed53cd080a200d38d10b4a90eac1a5143
gangeshsingh72 <singh.gangesh@gmail.com>
2020-06-28 21:23:23 +0100

    VariableScope

---
+++ src/VariableScope.java
@@ -0,0 +1,68 @@
+public class VariableScope {
+
+        static int myGlobalint = 5;
+//    static int myMainLocalint = 0;
+
+        public static void main(String[] args) {
+
+//        System.out.println(my);;
+
+            int myMainLocalint = 10;
+
+
+
+            System.out.println(myMainLocalint);
+            System.out.println(myGlobalint);
+            AnotherVariableScopeClass avsc = new AnotherVariableScopeClass();
+
+
+
+
+
+            AnotherVariableScopeClass abc = new AnotherVariableScopeClass();
+            abc.myStaticGlobalint=0;
+
+            avsc.myStaticGlobalint = 10;
+
+            abc.anotherMethod();
+
+
+
+            System.out.println(abc.myStaticGlobalint);
+
+            {
+                int myCodeblockInt = 100;
+
+                System.out.println(myCodeblockInt);
+            }
+//        System.out.println(m);
+//        System.out.println(myCodeblockInt);
+
+            VariableScope vs = new VariableScope();
+            vs.myMethod();
+
+
+        }
+
+        void myMethod(){
+            int myLocalInt = 10;
+
+
+
+//        System.out.println(myMainLocalint );
+
+            System.out.println(myGlobalint);
+
+        }
+
+    }
+
+    class AnotherVariableScopeClass{
+
+        int myStaticGlobalint;
+        public void anotherMethod(){
+
+        }
+
+
+}
